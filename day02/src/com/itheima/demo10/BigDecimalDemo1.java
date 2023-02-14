d1ec9195602df9ae433d2ab50f23ee50ca2cd341
DNYY-123 <2509184782@qq.com>
2023-02-15 00:42:48 +0800

    11

---
+++ day02/src/com/itheima/demo10/BigDecimalDemo1.java
@@ -0,0 +1,19 @@
+package com.itheima.demo10;
+
+import java.math.BigDecimal;
+
+public class BigDecimalDemo1 {
+    public static void main(String[] args) {
+        // BigDecimal bb3 = new BigDecimal("0.01");
+        // System.out.println(bb3);
+        BigDecimal bb4 = new BigDecimal("0.09");
+        // BigDecimal bb5 = bb4.add(bb3);
+        // System.out.println(bb5);
+        // System.out.println(BigDecimal.ROUND_CEILING);
+        BigDecimal bd1 = new BigDecimal("0.226");
+        BigDecimal bd2 = new BigDecimal("123.226");
+        BigDecimal bd3 = new BigDecimal("-1.5");
+    }
+
+
+}
